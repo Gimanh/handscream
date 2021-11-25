00194e6bcd52b74233c62c41e404b2c543cc83bd
nikolaygiman <gimanhead@gmail.com>
2021-11-25 21:45:03 +0300

    wip: bash script for creating component

---
+++ WebApp/TaskView/components/create-component.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+reset=`tput sgr0`
+
+if [ ! -e "./$1" ]; then
+    echo "${green}Start creating${reset}"
+
+    mkdir $1
+
+    echo "import { Component } from 'vue-property-decorator';
+    import AppBase from '~/components/AppBase';
+
+    @Component
+    export default class $1 extends AppBase {
+
+    }" > $1/$1.ts
+
+    echo "<template>
+        <h1>Component $1</h1>
+    </template>
+
+    <script src="./$1.ts" />" > $1/$1.vue
+
+    echo "import $1 from './$1.vue';
+
+    export { $1 };
+    export default $1;" > $1/index.ts
+
+    echo "export * from './$1';" >> index.ts
+
+    git add $1
+
+    echo "${green}File created! Use component ${yellow}<$1 />${green} in vue files.${reset}"
+else
+    echo "${red}File already exists! Can not create components!${reset}"
+fi
