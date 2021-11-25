37c013806b6dc57f08505f56499cc869e168aba7
nikolaygiman <gimanhead@gmail.com>
2021-11-25 22:00:50 +0300

    wip: check export in index.ts

--- WebApp/TaskView/components/create-component.sh
+++ WebApp/TaskView/components/create-component.sh
@@ -6,30 +6,36 @@ yellow=`tput setaf 3`
 reset=`tput sgr0`
 
 if [ ! -e "./$1" ]; then
-    echo "${green}Start creating${reset}"
+    echo "${green}Start creating component${reset}"
 
     mkdir $1
 
     echo "import { Component } from 'vue-property-decorator';
-    import AppBase from '~/components/AppBase';
+import AppBase from '~/components/AppBase';
 
-    @Component
-    export default class $1 extends AppBase {
+@Component
+export default class $1 extends AppBase {
 
-    }" > $1/$1.ts
+}" > $1/$1.ts
 
     echo "<template>
-        <h1>Component $1</h1>
-    </template>
+    <h1>Component $1</h1>
+</template>
 
-    <script src="./$1.ts" />" > $1/$1.vue
+<script src="./$1.ts" />" > $1/$1.vue
 
     echo "import $1 from './$1.vue';
 
-    export { $1 };
-    export default $1;" > $1/index.ts
+export { $1 };
+export default $1;" > $1/index.ts
 
-    echo "export * from './$1';" >> index.ts
+    if grep -Fxq "export * from './$1';" index.ts
+    then
+        echo "${yellow}Export in index.ts already exists!${reset}"
+    else
+        echo "export * from './$1';" >> index.ts
+        echo "${green}Export added into index.ts${reset}"
+    fi
 
     git add $1
 
