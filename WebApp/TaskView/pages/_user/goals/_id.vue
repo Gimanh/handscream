7008d1b6cc1f6e8822b1c6ad0e88c3f50ee328f1
nikolaygiman <gimanhead@gmail.com>
2021-12-16 22:20:42 +0300

    wip: ---

--- WebApp/TaskView/pages/_user/goals/_id.vue
+++ WebApp/TaskView/pages/_user/goals/_id.vue
@@ -6,10 +6,5 @@
         <tasks
             :component-id="$route.params.list"
         />
-        <!--        <div>tasks or OTHER for selected component {{ $route.params.id }}</div>
-                {{ $route.params }}
-                <div>
-                    <router-view />
-                </div>-->
     </v-col>
 </template>
