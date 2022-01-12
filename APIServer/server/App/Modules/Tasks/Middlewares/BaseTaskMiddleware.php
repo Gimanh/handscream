0a20dc680bf7c495be6ad55edee7a5336ed11184
nikolaygiman <gimanhead@gmail.com>
2022-01-12 20:47:28 +0300

    wip: middleware for permission check

---
+++ APIServer/server/App/Modules/Tasks/Middlewares/BaseTaskMiddleware.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Modules\Tasks\Middlewares;
+
+use RuntimeException;
+use ZXC\Modules\DB\DB;
+use ZXC\Native\Modules;
+use ZXC\Modules\Auth\User;
+use ZXC\Interfaces\Psr\Server\MiddlewareInterface;
+use ZXC\Interfaces\Psr\Server\RequestHandlerInterface;
+use ZXC\Interfaces\Psr\Http\Message\ResponseInterface;
+use ZXC\Interfaces\Psr\Http\Message\ServerRequestInterface;
+
+class BaseTaskMiddleware implements MiddlewareInterface
+{
+    protected ?DB $db = null;
+
+    protected ?User $user = null;
+
+    public function __construct()
+    {
+        $this->db = Modules::get('db');
+    }
+
+    protected function initUser(ServerRequestInterface $request)
+    {
+        $this->user = $request->getAttribute('user');
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        throw new RuntimeException('Implement process() method in ' . static::class . '.');
+    }
+}
