784a10ee9a8e9e293d569afcf2e713c041e65db7
nikolaygiman <gimanhead@gmail.com>
2022-02-24 19:03:34 +0300

    wip: styles

--- WebApp/TaskView/pages/index.vue
+++ WebApp/TaskView/pages/index.vue
@@ -1,8 +1,6 @@
 <template>
     <v-row justify="center" align="center" style="height: 100%;">
-        <v-col cols="6" sm="8" md="6" align-self="center">
-            <app-credentials-form />
-        </v-col>
+        <app-credentials-form />
     </v-row>
 </template>
 
