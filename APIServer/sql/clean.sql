6662369db54968706a590110a070f65d0d56f429
nikolaygiman <gimanhead@gmail.com>
2022-04-24 21:53:54 +0300

    ver: v1.1.0

--- APIServer/sql/clean.sql
+++
@@ -1,5 +0,0 @@
-drop schema if exists tv_auth cascade;
-drop schema if exists tasks cascade;
-drop schema if exists tasks_auth cascade;
-drop schema if exists about cascade;
-drop schema if exists app cascade;
